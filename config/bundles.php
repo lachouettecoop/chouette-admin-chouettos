53a8fdfa375e4518d992ebe018d3339b4a239121
Clément <contact@glukose.fr>
2020-11-07 10:57:58 +0100

    feat: API platform install and graphql support

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,6 @@ return [
     Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
     Sonata\AdminBundle\SonataAdminBundle::class => ['all' => true],
     Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
