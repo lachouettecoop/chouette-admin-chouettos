fb9bdac26285605e9e2d4da60ed86f16e3900853
Erik Aouizerate <erik.aouizerate-ext@mfi.fr>
2023-05-30 20:56:50 +0200

    fix: Remove user from ldap when essai is finished

--- src/Controller/PlanningController.php
+++ src/Controller/PlanningController.php
@@ -170,6 +170,8 @@ class PlanningController extends AbstractController
             if ($date > $essai && $user->getEnabled()) {
                 $user->setEnabled(false);
                 $em->persist($user);
+                $this->ldapService->removeUserOnLDAP($user);
+
                 $emailContent = $this->renderView('planning/notificationEssaiNotInterested.html.twig', []);
                 $this->sendEmail("Informations de votre fin de période d'essai", $user->getEmail(), $emailContent, $mailer);
             }
